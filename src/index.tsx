db27794bbfe561d812e80e53d726040973ce1fa9
Ravalison Manda <manda.ravalison@gmail.com>
2021-08-03 22:15:47 +0300

    Déplacement des styles par défaut dans un nouveau fichier default.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/default.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
