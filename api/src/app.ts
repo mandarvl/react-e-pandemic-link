14749716a1daebac2658a87a3a06f8e5e3db4e69
Ravalison Manda <manda.ravalison@gmail.com>
2021-08-16 05:13:32 +0300

    Mise à jour de gitignore et création du serverside

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,16 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import createError from 'http-errors' ;
+import express, { Request, Response, NextFunction} from 'express' ;
+import path from 'path' ;
+import cookieParser from 'cookie-parser' ;
+import logger from 'morgan' ;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+import testAPIRouter from './routes/testApi';
 
-var app = express();
+import { HttpError } from 'http-errors';
+
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -28,7 +32,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function(err: HttpError, req: Request, res: Response, next: NextFunction) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +42,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+export default app;
---
+++ client/src/Components/Test/Test.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react' ;
+
+class Test extends Component{
+    render(){
+        return (<p></p>) ;
+    }
+}
+
+export default Test ;
